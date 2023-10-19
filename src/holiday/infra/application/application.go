e981ec7814e2ca92368c5433350955679ff3780c
Jonnatan Oyarzun <joyarzun@users.noreply.github.com>
2023-10-19 17:31:38 -0300

    chore: removes registry to simplify arquitecture

--- src/holiday/infra/application/application.go
+++ src/holiday/infra/application/application.go
@@ -6,14 +6,24 @@ import (
 	"github.com/labstack/echo"
 	"gitlab.com/joyarzun/go-clean-architecture/src/holiday/infra/datastore"
 	"gitlab.com/joyarzun/go-clean-architecture/src/holiday/infra/router"
-	"gitlab.com/joyarzun/go-clean-architecture/src/holiday/registry"
+	"gitlab.com/joyarzun/go-clean-architecture/src/holiday/interfaceadapter/controller"
+	"gitlab.com/joyarzun/go-clean-architecture/src/holiday/interfaceadapter/presenter"
+	"gitlab.com/joyarzun/go-clean-architecture/src/holiday/interfaceadapter/repository"
+	"gitlab.com/joyarzun/go-clean-architecture/src/holiday/usecases"
 )
 
 func Run() {
 	port := "3000"
 
-	reg := registry.NewRegistry(datastore.New(datastore.DBFILE))
-	server := router.NewRouter(echo.New(), reg.NewAppController())
+	holidayRepository := repository.New(datastore.New(datastore.DBFILE))
+	holidayPresenter := presenter.New()
+	holidayService := usecases.New(&holidayRepository, &holidayPresenter)
+
+	mainController := controller.AppController{
+		Holiday: controller.New(holidayService),
+	}
+
+	server := router.NewRouter(echo.New(), mainController)
 	server.Logger.Fatal(server.Start(":" + port))
 	fmt.Println("Server listen at http://localhost" + ":" + port)
 }
